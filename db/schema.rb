90923f3ed982105f503c40e965c80ce755357f6f
Chris Ma <crispy-studios@hotmail.com>
2014-12-09 16:59:00 +1100

    updates from devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20141209044416) do
-
 
   create_table "activities", force: true do |t|
     t.string   "name"
